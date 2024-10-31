1cb1456ea3a4522f0d4ba4789b9e7492c62f0617
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-10-31 17:11:45 +0100

    Drag and Drop
    -drag and drop enter object

--- Assets/script/Inventory/InventoryPlayerManager.cs
+++ Assets/script/Inventory/InventoryPlayerManager.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class InventoryPlayerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _inventoryPlayerCase;
-    [SerializeField] private List<Scriptable_Ressources> _scripts = new List<Scriptable_Ressources>();
-    [SerializeField] private List<GameObject> _objects = new List<GameObject>();
+    [SerializeField] private RectTransform _inventoryParentSlot; 
 
-    [SerializeField] private List<int> _quantities = new List<int>();
+    [SerializeField] private List<Scriptable_Ressources> _scripts = new List<Scriptable_Ressources>(); 
+    [SerializeField] private List<GameObject> _objects = new List<GameObject>(); 
+    [SerializeField] private List<GameObject> _inventoryPlayerSlots = new List<GameObject>(); 
 
     void Start()
     {
@@ -18,48 +18,18 @@ public class InventoryPlayerManager : MonoBehaviour
     {
         for (int i = 0; i < _scripts.Count; i++)
         {
-            GameObject itemInstance = Instantiate(_inventoryPlayerCase);
-            Ressource ressourceComponent = itemInstance.AddComponent<Ressource>();
-
-            int itemQuantity = (i < _quantities.Count) ? _quantities[i] : 1; 
-            ressourceComponent.SetAllParameters(_scripts[i], itemQuantity);
-
-            _objects.Add(itemInstance);
-        }
-        RecupAllInfo();
-    }
-
-    public void RecupAllInfo()
-    {
-        for (int i = 0; i < _objects.Count; i++)
-        {
-            Ressource ressource = _objects[i].GetComponent<Ressource>();
-            if (ressource != null)
-            {
-                ressource.SetAllParameters(_scripts[i], ressource.Quantity);
-            }
+            GameObject selectedPrefab = _inventoryPlayerSlots[i % _inventoryPlayerSlots.Count]; 
+            GameObject itemInstance = Instantiate(selectedPrefab, _inventoryParentSlot);
+            InvRessource ressourceComponent = itemInstance.AddComponent<InvRessource>();
+            ressourceComponent.Ressource = _scripts[i]; 
+            ressourceComponent.Quantity = i + 1; 
+            _objects.Add(itemInstance); 
         }
     }
 }
 
-public class Ressource : MonoBehaviour
+public class InvRessource : MonoBehaviour
 {
-    public int Id;
-    public string Name;
-    public Sprite Sprite;
-    public bool IsPure;
-    public ScriptableObject GrounderOutput;
-    public List<ScriptableObject> DisassenblerOutputs;
-    public int Quantity;
-
-    public void SetAllParameters(Scriptable_Ressources ressources, int quantity)
-    {
-        Id = ressources.Id;
-        Name = ressources.Name;
-        Sprite = ressources.Sprite;
-        IsPure = ressources.IsPure;
-        GrounderOutput = ressources.GrinderOutput;
-        DisassenblerOutputs = ressources.DisassemblerOutputs;
-        Quantity = quantity; 
-    }
+    public Scriptable_Ressources Ressource; 
+    public int Quantity; 
 }
