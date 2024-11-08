76831fc2c183dd6413999deba12616a8b518741c
matcodeew <150682957+matcodeew@users.noreply.github.com>
2024-11-08 16:50:51 +0100

    init

---
+++ Assets/script/Convoyers/TileSelection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelection : MonoBehaviour
+{
+    private List<TileData> tilesSelectioned = new();
+
+
+}
