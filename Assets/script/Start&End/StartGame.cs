48b45e4af82f134f94141fcb29198b2c40aa8ac9
matcodeew <150682957+matcodeew@users.noreply.github.com>
2024-11-14 14:24:23 +0100

    init and add shader
    shader not work

---
+++ Assets/script/Start&End/StartGame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private GameObject SpaceShipPrefab;
+    [SerializeField] private GameObject NavettePrefab;
+
+} 
