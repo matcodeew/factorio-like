666d7ccd1c814ca60be93658207cc0ef833e22e2
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-11-18 23:39:20 +0100

    fix camera controller

--- Assets/script/CameraController.cs
+++ Assets/script/CameraController.cs
@@ -10,8 +10,7 @@ public class CameraController : MonoBehaviour
     private float _maxSpeed = 50f;
     private float _accelerationRate = 20f;
     private float _speed = 30f;
-    private float _border = 50.0f;
-    private float _activationBorder = 10.0f; // Nouvelle marge pour l'activation du mouvement
+    private float _border = 50.0f; // La zone de la bordure où la caméra commence à se déplacer
     private float _accelerationX = 5f;
     private float _accelerationY = 5f;
 
@@ -56,16 +55,16 @@ public class CameraController : MonoBehaviour
         {
             Vector3 newPosition = _cam.transform.position;
 
-            // Vérifier si la souris est en dehors de l'écran à droite
-            if (Input.mousePosition.x >= Screen.width + _border)
+            // Vérification du bord droit
+            if (Input.mousePosition.x >= Screen.width - _border)
             {
                 _accelerationX += _accelerationRate * Time.deltaTime;
                 _accelerationX = Mathf.Min(_accelerationX, _maxSpeed);
 
                 newPosition.x += _accelerationX * Time.deltaTime;
             }
-            // Vérifier si la souris est en dehors de l'écran à gauche
-            else if (Input.mousePosition.x <= -_border)
+            // Vérification du bord gauche
+            else if (Input.mousePosition.x <= _border)
             {
                 _accelerationX += _accelerationRate * Time.deltaTime;
                 _accelerationX = Mathf.Min(_accelerationX, _maxSpeed);
@@ -77,16 +76,16 @@ public class CameraController : MonoBehaviour
                 _accelerationX = 0f;
             }
 
-            // Vérifier si la souris est en dehors de l'écran en haut
-            if (Input.mousePosition.y >= Screen.height + _border)
+            // Vérification du bord du haut
+            if (Input.mousePosition.y >= Screen.height - _border)
             {
                 _accelerationY += _accelerationRate * Time.deltaTime;
                 _accelerationY = Mathf.Min(_accelerationY, _maxSpeed);
 
                 newPosition.z += _accelerationY * Time.deltaTime;
             }
-            // Vérifier si la souris est en dehors de l'écran en bas
-            else if (Input.mousePosition.y <= -_border)
+            // Vérification du bord du bas
+            else if (Input.mousePosition.y <= _border)
             {
                 _accelerationY += _accelerationRate * Time.deltaTime;
                 _accelerationY = Mathf.Min(_accelerationY, _maxSpeed);
@@ -98,7 +97,7 @@ public class CameraController : MonoBehaviour
                 _accelerationY = 0f;
             }
 
-            // Clamper les limites de la caméra
+            // Clamper les limites de la caméra pour éviter de sortir de la zone définie
             newPosition.x = Mathf.Clamp(newPosition.x, MinX, MaxX);
             newPosition.y = Mathf.Clamp(newPosition.y, MinY, MaxY);
 
